c78338b76ec7e8d001a61443df16f122754fbac6
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-24 23:07:38 +0530

    git add . git commit -m "chore: remove dist and node_modules from version control"

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const greet = (name) => {
-    return `Hello, ${name}!`;
-};
-console.log(greet("Sony"));
-//# sourceMappingURL=index.js.map
