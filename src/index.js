dbabd2f763481219bdc845029a6cdbb1ec3ba4eb
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-24 22:57:47 +0530

    build: compile TypeScript to JavaScript output in dist folder

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+console.log(greet("Sony"));
+//# sourceMappingURL=index.js.map
