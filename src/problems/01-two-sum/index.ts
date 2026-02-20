edc65f6b09b47bbf391d7b32cc846ee0f5f4e1ea
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-02-20 22:40:19 +0530

    implement two sum (leetcode problem)

--- src/problems/01-two-sum/index.ts
+++ src/problems/01-two-sum/index.ts
@@ -31,19 +31,14 @@
 console.log("Running two-sum problem...");
 
 function twoSum(nums: number[], target: number): number[] {
-    const map = new Map<number, number>();
-
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-
-        if (map.has(complement)) {
-            return [map.get(complement)!, i];
+   for(let i=0; i<nums.length; i++){
+    for(let j=i+1; j<nums.length; j++){
+        if(nums[i]+nums[j]=== target){
+            return [i, j];
         }
-
-        map.set(nums[i], i);
     }
-
-    return [];
+   }
+   return [];
 }
 
 
